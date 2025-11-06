1b596bcb8acf5e0d28ded0f84239b65c649aa287
XyyCi1 <bavikx@gmail.com>
2025-11-06 23:13:40 +0800

    file

--- Laboratory Exercise 10_Navarrete/Form1.Designer.vb
+++ Laboratory Exercise 10_Navarrete/Form1.Designer.vb
@@ -22,17 +22,64 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.input = New System.Windows.Forms.TextBox()
+        Me.ShowList = New System.Windows.Forms.Button()
+        Me.ClearList = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(319, 199)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Add to list"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'input
+        '
+        Me.input.Location = New System.Drawing.Point(306, 154)
+        Me.input.Name = "input"
+        Me.input.Size = New System.Drawing.Size(100, 20)
+        Me.input.TabIndex = 1
+        '
+        'ShowList
+        '
+        Me.ShowList.Location = New System.Drawing.Point(319, 242)
+        Me.ShowList.Name = "ShowList"
+        Me.ShowList.Size = New System.Drawing.Size(75, 23)
+        Me.ShowList.TabIndex = 2
+        Me.ShowList.Text = "Show list"
+        Me.ShowList.UseVisualStyleBackColor = True
+        '
+        'ClearList
+        '
+        Me.ClearList.Location = New System.Drawing.Point(437, 228)
+        Me.ClearList.Name = "ClearList"
+        Me.ClearList.Size = New System.Drawing.Size(75, 23)
+        Me.ClearList.TabIndex = 3
+        Me.ClearList.Text = "Clear List"
+        Me.ClearList.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ClearList)
+        Me.Controls.Add(Me.ShowList)
+        Me.Controls.Add(Me.input)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents input As TextBox
+    Friend WithEvents ShowList As Button
+    Friend WithEvents ClearList As Button
 End Class
--- Laboratory Exercise 10_Navarrete/Form1.vb
+++ Laboratory Exercise 10_Navarrete/Form1.vb
@@ -1,5 +1,62 @@
-﻿Public Class Form1
+﻿Imports System.IO
+
+Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Dim numbers As New List(Of Integer)
+    Dim filepath As String = "numbers.txt"
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        Dim verify As Boolean = Double.TryParse(input.Text, Nothing)
+
+        If verify Then
+
+            Using writer As New StreamWriter(filepath, True)
+                writer.WriteLine(input.Text)
+            End Using
+
+
+            numbers.Clear()
+            Using reader As New StreamReader(filepath)
+                While Not reader.EndOfStream
+
+                    Dim line As String = reader.ReadLine()
+
+                    If Integer.TryParse(line, Nothing) Then
+                        numbers.Add(Convert.ToInt32(line))
+                    End If
+                End While
+            End Using
+
+            MessageBox.Show($"{input.Text} saved successfully.")
+        Else
+            MessageBox.Show("Please enter a valid number.")
+
+        End If
+
+    End Sub
+
+    Private Sub ShowList_Click(sender As Object, e As EventArgs) Handles ShowList.Click
+
+
+
+
+        Dim print As String = "Numbers in the list:" & Environment.NewLine
+
+        Dim sorted = numbers.OrderByDescending(Function(x) x)
+
+        For Each item In sorted
+            print += item & Environment.NewLine
+        Next
+
+        MessageBox.Show(print)
+    End Sub
+
+    Private Sub ClearList_Click(sender As Object, e As EventArgs) Handles ClearList.Click
+        numbers.Clear()
+        File.WriteAllText(filepath, "")
     End Sub
 End Class
